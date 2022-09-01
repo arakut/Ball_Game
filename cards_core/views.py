0b263abe21a173c883dd95e4f2204fa15fa7f293
Artur <105674657+arakut@users.noreply.github.com>
2022-09-01 17:44:58 +0300

    Добавил яндекс.карты при просмотре информации о площадке

--- cards_core/views.py
+++ cards_core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from cards_core.models import Playground
+import folium
 # Create your views here.
 
 class ListObjectsView(ListView):
@@ -28,4 +29,5 @@ class PGUpdateView(UpdateView):
 class PGDeleteView(DeleteView):
     model = Playground
     success_url = '/'
-    template_name = 'delet_pg.html'
+    template_name = 'ymaps.html'
+
